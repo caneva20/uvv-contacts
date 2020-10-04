ab2abedaa5386ed736f88ec0832ad5bb0744d1ea
Fabricio Caneva <hardlymersk@gmail.com>
2020-10-04 12:55:21 -0300

    add logging utils

---
+++ uvv-contacts/Logging/ILogger.cs
@@ -0,0 +1,8 @@
+﻿namespace caneva20.Logging {
+    public interface ILogger {
+        void Debug(string message);
+        void Info(string message);
+        void Warning(string message);
+        void Error(string message);
+    }
+}
---
+++ uvv-contacts/Logging/Logger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace caneva20.Logging {
+    public class Logger : ILogger {
+        public void Debug(string message) {
+            Console.WriteLine($"[DEBUG] {message}");
+        }
+
+        public void Info(string message) {
+            Console.WriteLine($" [INFO] {message}");
+        }
+
+        public void Warning(string message) {
+            Console.WriteLine($" [WARN] {message}");
+        }
+
+        public void Error(string message) {
+            Console.WriteLine($"[ERROR] {message}");
+        }
+    }
+}
