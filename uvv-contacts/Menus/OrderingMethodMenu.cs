763f6db7a3fec45b5e2e95e7a187ba801c0c7f8f
Fabricio Caneva <hardlymersk@gmail.com>
2020-10-04 16:25:16 -0300

    add ordering selection UI

---
+++ uvv-contacts/Menus/OrderingMethodMenu.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using caneva20.Models;
+using caneva20.PromptEnhanced;
+
+namespace caneva20.Menus {
+    public static class OrderingMethodMenu {
+        public static Func<Contact, object> Show() {
+            return PromptE.Select<Func<Contact, object>>("Ordering method")
+               .Option("First name", x => x.FirstName)
+               .Option("Middle name", x => x.MiddleName)
+               .Option("Last name", x => x.LastName)
+               .Option("Email", x => x.Email)
+               .Option("Cellphone number", x => x.CellphoneNumber)
+               .Option("Cancel", null)
+               .Show();
+        }
+    }
+}
