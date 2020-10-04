058f043d66cd8709838f6f8efafa8251b97c89b3
Fabricio Caneva <hardlymersk@gmail.com>
2020-10-04 12:53:44 -0300

    add cellphone utils

---
+++ uvv-contacts/Utils/CellphoneUtils.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace caneva20.Utils {
+    public static class CellphoneUtils {
+        private const string CELLPHONE_REGEX = @"^\+?(\d{2}) ?\(?(\d{2})\)? ?(\d{4})[ -]?(\d{5})$";
+
+        public static bool IsValid(string cellphone) {
+            return Regex.IsMatch(cellphone, CELLPHONE_REGEX);
+        }
+
+        public static string Parse(string cellphone) {
+            return Regex.Replace(cellphone, CELLPHONE_REGEX, "+$1 ($2) $3-$4");
+        }
+    }
+}
