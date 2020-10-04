9451c8e843fbf52ef6665896f389e4bf59b91daf
Fabricio Caneva <hardlymersk@gmail.com>
2020-10-04 15:49:47 -0300

    add main menu UI

---
+++ uvv-contacts/Menus/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using caneva20.Menus.Utils;
+using caneva20.PromptEnhanced;
+
+namespace caneva20.Menus {
+    public static class MainMenu {
+        public static void Show(ContactBook book) {
+            while (true) {
+                PromptE.Menu("[UVV Contacts] Pick an option")
+                   .Action(new ExitApplicationAction())
+                   .Show();
+            }
+            
+            // ReSharper disable once FunctionNeverReturns
+        }
+    }
+}
