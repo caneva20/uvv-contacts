3bf3a13fefbd48c78e2426d20fa400e9ccea7ba2
Fabricio Caneva <hardlymersk@gmail.com>
2020-10-04 18:01:59 -0300

    add startup code

--- uvv-contacts/Program.cs
+++ uvv-contacts/Program.cs
@@ -1,18 +1,46 @@
-﻿using System.Threading.Tasks;
-using caneva20.CircularList;
+﻿using System.Collections.Generic;
+using Autofac;
+using caneva20.Logging;
+using caneva20.Menus;
+using caneva20.Models;
+using caneva20.Persistence;
+using caneva20.Persistence.Readers;
+using caneva20.Persistence.Serializer;
+using caneva20.Persistence.Writers;
 
 namespace caneva20 {
     class Program {
-        private static async Task Main(string[] args) {
-            var list = new CircularList<int>();
+        private const string CONTACTS_BOOK_PATH = "C:/Users/Jesus/Desktop/out/book.json";
 
-            for (var i = 0; i < 10; i++) {
-                list.Add(i);
-            }
+        private static void Main(string[] args) {
+            var container = BuildDIContainer();
             
-            list.Remove(5);
+            var book = container.Resolve<ContactBook>();
 
-            var a = "";
+            MainMenu.Show(book);
+        }
+
+        private static IContainer BuildDIContainer() {
+            var diBuilder = new ContainerBuilder();
+
+            diBuilder.RegisterType<Logger>().As<ILogger>();
+
+            diBuilder.RegisterType<FileWriter>()
+               .As<IWriter>()
+               .WithParameter(new TypedParameter(typeof(string), CONTACTS_BOOK_PATH));
+
+            diBuilder.RegisterType<FileReader>()
+               .As<IReader>()
+               .WithParameter(new TypedParameter(typeof(string), CONTACTS_BOOK_PATH));
+
+            diBuilder.RegisterType<JsonSerializer<IEnumerable<Contact>>>()
+               .As<ISerializer<IEnumerable<Contact>>>();
+            diBuilder.RegisterType<Persister<IEnumerable<Contact>>>()
+               .As<IPersister<IEnumerable<Contact>>>();
+
+            diBuilder.RegisterType<ContactBook>();
+
+            return diBuilder.Build();
         }
     }
 }
